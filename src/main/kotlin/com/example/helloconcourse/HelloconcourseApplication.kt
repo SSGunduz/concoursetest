6dcc359eab25cc44572cda4ec777f79358d93498
serdar.suat <serdar.suat@hexad.de>
2018-12-18 09:55:11 +0100

    initial commit

---
+++ src/main/kotlin/com/example/helloconcourse/HelloconcourseApplication.kt
@@ -0,0 +1,12 @@
+package com.example.helloconcourse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloconcourseApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelloconcourseApplication>(*args)
+}
+
